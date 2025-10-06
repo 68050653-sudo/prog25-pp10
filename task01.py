1aee4f51d321e50282c99e38a96dd786fbf0054a
68050653-sudo <68050653@kmitl.ac.th>
2025-10-06 20:11:19 +0700

    Initial commit

---
+++ task01.py
@@ -0,0 +1,31 @@
+"""
+PROG 2025/1
+Pair Programming Assignment #10
+Task #1
+"""
+
+# Do not remove this line!
+from poomdt import is_leap_year
+
+# Test 1 - Test whether is_leap_year can handle 0 correctly
+def test_year_is_zero():
+    if is_leap_year(0) == True:
+        return True
+    else:
+        return False
+
+# Test 2 - ...
+def test_2():
+    pass
+
+# Test 3 - ...
+def test_3():
+    pass
+
+# Test 4 - ...
+def test_4():
+    pass
+
+# Test 5 - ...
+def test_5():
+    pass
